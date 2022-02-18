8d4bffd670886032dc8f72a7485746efca04d326
thiag <thiag@NOTETHIAGO>
2022-02-18 15:25:07 -0300

    Add project files.

---
+++ CasoFlaviusJosephus.ConsoleApp/CasoFlaviusJosephus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CasoFlaviusJosephus.ConsoleApp
+{
+    internal class CasoFlaviusJosephus
+    {
+        static void Main(string[] args)
+        {
+            int numerodepesoas = 0;
+            int numerodopulo = 0;
+            string [] entrada ;
+            int[] pessoastotal;
+
+            Console.WriteLine("Suicidio coletivo");
+            Console.WriteLine("Digite o numero de pessoas e o número de pulo:");
+            entrada = Console.ReadLine().Split(" ");
+            #region separando as entradas
+            for (int i = 0; i <= entrada.Length; i++)
+            {
+                if (i == 0)
+                {
+                    numerodepesoas = Convert.ToInt32(entrada[i]);
+                }
+                if (i == 1)
+                {
+                    numerodopulo = Convert.ToInt32(entrada[i]);
+                }
+                
+            }
+            #endregion
+
+            pessoastotal = new int [numerodepesoas];
+
+            for(int i = 0;i < numerodepesoas; i++)
+            {
+                pessoastotal = new int [i];
+                Console.WriteLine(pessoastotal[i]);
+            }
+        }
+    }
+}
---
+++ SegundaProva/Esquerda volver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace SegundaProva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodecomando = 0;
+            string comandosrecebidos = "";
+            string lado = "N";
+            string opcao = "1";
+
+            Console.WriteLine("Digite o tanto de comandos: ");
+            numerodecomando = Convert.ToInt32(Console.ReadLine());
+
+            while (opcao == "1")
+            {
+                if (numerodecomando > 0)
+                {
+                    Console.WriteLine("Digite os comandos: ");
+                    comandosrecebidos = Console.ReadLine();
+                    comandosrecebidos = comandosrecebidos.ToUpper();
+
+
+
+
+
+                    foreach (char comando in comandosrecebidos.ToCharArray())
+                    {
+                        #region lado esquerdo
+                        if (comando == 'E' && lado == "N")
+                        {
+                            lado = "O";
+                        }
+                        else if (comando == 'E' && lado == "O")
+                        {
+                            lado = "S";
+                        }
+                        else if (comando == 'E' && lado == "S")
+                        {
+                            lado = "L";
+                        }
+                        else if (comando == 'E' && lado == "L")
+                        {
+                            lado = "N";
+                        }
+                        #endregion
+
+                        #region lado direito
+                        if (comando == 'D' && lado == "N")
+                        {
+                            lado = "L";
+                        }
+                        else if (comando == 'D' && lado == "O")
+                        {
+                            lado = "N";
+                        }
+                        else if (comando == 'D' && lado == "S")
+                        {
+                            lado = "O";
+                        }
+                        else if (comando == 'D' && lado == "L")
+                        {
+                            lado = "S";
+                        }
+                        #endregion
+
+
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                
+                     Console.WriteLine(lado);
+                Console.WriteLine("Tabela de opções");
+                Console.WriteLine("Digite 1 para repetir");
+                Console.WriteLine("presione enter para sair");
+                opcao = Console.ReadLine();
+            }
+
+           
+
+
+
+        }
+    }
+}
